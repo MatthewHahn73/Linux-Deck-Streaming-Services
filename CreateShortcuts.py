1fbf8796274203df368c8fd2c0b37109dd7c602f
Matthew Hahn <45131687+MatthewHahn73@users.noreply.github.com>
2025-04-22 14:01:00 -0700

    Refactoring
    Changed how some values are fetched to be more readable and maintainable

--- CreateShortcuts.py
+++ CreateShortcuts.py
@@ -5,25 +5,33 @@ Current Bugs
     -Steam doesn't populate the non-steam games with the right artwork. Likely due to steam using some kind of hashing when normally storing those values
 
 TODO
-    -Need to compact all the logic into one script. Move Setup.sh logic to here
     -Need to add in better documentation of current script progress and exception handling
     -Need to better optimize the GrabAndSetArtworkForGivenAppIDs function 
-    -Need to find a better way to get the StreamingServices and ArtworkLinks values 
 """
 
 import os, binascii, json, logging
 from urllib.request import Request, urlopen
 
-SteamUserData = "/home/" + os.getlogin() + "/.steam/steam/userdata/"
+GlobalLogger = logging.getLogger()
+GlobalLogger.setLevel(logging.DEBUG)
+
+User = os.getlogin()
+SteamUserData = "/home/" + User + "/.steam/steam/userdata/"
+SteamGameData = "/home/" + User + "/.steam/steam/steamapps/common"
 PathToUserConfig = SteamUserData + os.listdir(SteamUserData)[0] + "/config/grid/"
 PathToSteamShortcutsFile = SteamUserData + os.listdir(SteamUserData)[0] + "/config/shortcuts.vdf"
 
+def GenerateAppID(ExeName, AppName):
+    UniqueID = "".join([ExeName, AppName])
+    AppID = binascii.crc32(str.encode(UniqueID)) | 0x80000000
+    return AppID
+
 def GrabAndSetArtworkForGivenAppIDs(ArtworkLinks, AppIDs):
     for StreamingAppType in ArtworkLinks: 
-        for Artwork in ArtworkLinks[StreamingAppType]:
-            #Set the artwork values to something usable
-            ArtworkType = Artwork.replace(" - Horizontal", "").replace(" - Vertical", "")
-            ArtworkURL = "https://cdn2.steamgriddb.com/" + ArtworkType + "/" + ArtworkLinks[StreamingAppType][Artwork]
+        for ArtworkType in ArtworkLinks[StreamingAppType]["Artwork"]:
+            #Set the artwork values to something usable and query steamgriddb for the artwork
+            ArtworkTypeParsed = ArtworkType.replace(" - Horizontal", "").replace(" - Vertical", "")
+            ArtworkURL = "https://cdn2.steamgriddb.com/" + ArtworkTypeParsed + "/" + ArtworkLinks[StreamingAppType]["Artwork"][ArtworkType]
             ArtworkFileName, ArtworkFileType = os.path.splitext(ArtworkURL)
             ArtworkSuffix = {
                 "grid - Vertical": "p",
@@ -31,43 +39,39 @@ def GrabAndSetArtworkForGivenAppIDs(ArtworkLinks, AppIDs):
                 "hero": "_hero", 
                 "logo": "_logo",
                 "icon": "_icon"
-            }.get(Artwork, "")
-            #Query steamgriddb for the artwork
+            }.get(ArtworkType, "")
+            logging.info("Fetching '" + StreamingAppType + "' artwork at '" + ArtworkURL + "' ...")
             SteamGridDBRequest = Request(url=ArtworkURL, headers={
                             'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
                             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
                             'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
                             'Accept-Encoding': 'none',
                             'Accept-Language': 'en-US,en;q=0.8',
-                            'Connection': 'keep-alive'
+                            'Connection': 'close'
                         })
             ReturnedValue = urlopen(SteamGridDBRequest).read()
+
             #Create the files in the users userdata/<UserID>/grid/ directory, set to the appropriate AppID
             FullFileName = AppIDs[StreamingAppType] + ArtworkSuffix + ArtworkFileType
             with open(PathToUserConfig + FullFileName, "wb") as FetchedFile:
                 FetchedFile.write(ReturnedValue)
     
-def GenerateAppID(ExeName, AppName):
-    UniqueID = "".join([ExeName, AppName])
-    AppID = binascii.crc32(str.encode(UniqueID)) | 0x80000000
-    return AppID
-
 def GenerateVDFEntriesAndReturnAppIDs(StreamingServices):
-    PathToScript = "bash " + '"/home/deck/.steam/steam/steamapps/common/Streaming/LaunchApp.sh"'
-    PathToDirectory = '"/home/deck/.steam/steam/steamapps/common/Streaming/"'
+    PathToScript = "bash " + '"/home/' + User + '/.steam/steam/steamapps/common/Streaming/LaunchApp.sh"'
+    PathToDirectory = '"/home/' + User + '/.steam/steam/steamapps/common/Streaming/"'
 
-    #Check for an existing shortcuts.vdf, if doesn't exist, create one
+    #Check for an existing shortcuts.vdf file, and if doesn't exist, create one. Determine how many non-steam games already exist
     if not os.path.exists(PathToSteamShortcutsFile):
+        logging.info("Creating shortcuts file at '" + PathToSteamShortcutsFile + "'")
         with open(PathToSteamShortcutsFile, "wb") as NewlyCreatedFile:
             NewlyCreatedFile.write(b'\x00' + b'shortcuts' + b'\x00\x08\x08')
-       
-    #Open the shortcuts.vdf file for its contents and determine how many non-steam games already exist
     with open(PathToSteamShortcutsFile, "rb") as SteamShortcutsFile:
         SteamShortcutsFileContentsTrimmed = SteamShortcutsFile.readlines()[0][:-2]
         CurrentIteration = SteamShortcutsFileContentsTrimmed.count(b'appid')
         CurrentIteration += 1 if CurrentIteration != 0 else 0
 
     #For each streaming service, create a non-steam game and populate its attributes
+    logging.info("Appending new items to '" + PathToSteamShortcutsFile + "' ...")
     TotalContentsToAppend = b''
     CurrentAppIDList = {}
     for Key in StreamingServices:
@@ -76,7 +80,7 @@ def GenerateVDFEntriesAndReturnAppIDs(StreamingServices):
         AppNameBytes = str(Key).encode('utf-8')                                        
         PathToScriptBytes = str(PathToScript).encode('utf-8')                            
         StartDirectoryBytes = str(PathToDirectory).encode('utf-8')      
-        LaunchOptionsBytes = str('"' + str(StreamingServices[Key]) + '"').encode('utf-8')   
+        LaunchOptionsBytes = str('"' + str(StreamingServices[Key]["Link"]) + '"').encode('utf-8')   
 
         CurrentRow = b'\x00' + Iteration + b'\x00' + \
                         b'\x02' + b'appid' + b'\x00' + CurrentAppID + b'\x00' + \
@@ -108,47 +112,14 @@ def GenerateVDFEntriesAndReturnAppIDs(StreamingServices):
     return CurrentAppIDList
 
 if __name__ == "__main__":
-    AppIDs = GenerateVDFEntriesAndReturnAppIDs({
-        "Prime Video" : "https://www.amazon.com/video",
-        "Youtube" : "https://www.youtube.com/", 
-        "Netflix" : "https://www.netflix.com/", 
-        "HBO Max" : "https://www.max.com/", 
-        "Apple TV" : "https://tv.apple.com/"
-    })
-    GrabAndSetArtworkForGivenAppIDs({
-        "Prime Video" : {
-            "grid - Vertical" : "10c8f12e1fb3ed73b09ec0311a269a42.png", 
-            "grid - Horizontal" : "e152ea66175587989ee431b09e85b337.png", 
-            "hero" : "304cb49033bca92dc8391e133ef1d750.png", 
-            "logo" : "58840eb65da053fbdea5f4d19dd3e00f.png", 
-            "icon" : "f64b2463cf1dba199491c885dff932f3.ico"
-        }, 
-        "Youtube" : {
-            "grid - Vertical" : "f7df4b3972375bc784cd61ada708046f.jpg", 
-            "grid - Horizontal" : "98f67d0c03e88f22c2d9f2930848b8fa.jpg", 
-            "hero" : "4a9f57c5dadf5bc6555a2e754ca3cfa7.png", 
-            "logo" : "9216f34dc82ae586c1d6c37bdb8e8edf.png", 
-            "icon" : "995665640dc319973d3173a74a03860c.ico"
-        }, 
-        "Netflix" : {
-            "grid - Vertical" : "8db700701ae6ebeb663f433c35abeeb8.png", 
-            "grid - Horizontal" : "befe326073e97f6b4335c72e186b7369.jpg", 
-            "hero" : "d810302a6bb9bc97e7c9662b90a634a9.png", 
-            "logo" : "92757b7ebf6d8edfef5e211249a38ab5.png", 
-            "icon" : "4a06d868d044c50af0cf9bc82d2fc19f.ico"
-        }, 
-        "HBO Max" : {
-            "grid - Vertical" : "27d049f461720c8fbc869279e7da7e41.png", 
-            "grid - Horizontal" : "f045c10f3a39ac512783e0869bc45eb5.png", 
-            "hero" : "d6f07f83059edd69be854095a7ddd520.png", 
-            "logo" : "60a399eedc5ad40dbf3cd69cf3c178a6.png", 
-            "icon" : "501fe32ad8686901bb423ede39d6ce04.png"
-        }, 
-        "Apple TV" : {
-            "grid - Vertical" : "eff7b4b3d1cbcdc29ff8b8412d86af56.jpg", 
-            "grid - Horizontal" : "58d824c17ac25bd43fb42b087578071d.png", 
-            "hero" : "f39ab4e4f1135fd68862f122b6b22dd5.png", 
-            "logo" : "e637c9c15d72fcbeecdcf3bddc224054.png", 
-            "icon" : "7b1223235e9b545dffd56c4cac714b41.png"
-        }
-    }, AppIDs)
+    logging.info("Installing firefox ...")
+    #os.system("flatpak install flathub org.mozilla.firefox -y")
+
+    logging.info("Moving the files to '" + SteamGameData + "' ...")
+    #os.makedirs(SteamGameData + "/Streaming")
+    #os.system("cp -a $(pwd)/Streaming " + "'" + SteamGameData + "'")
+
+    with open("Script Assets/StreamingServices.json", "r") as StreamingFile:
+        StreamingServices = json.load(StreamingFile)
+    AppIDs = GenerateVDFEntriesAndReturnAppIDs(StreamingServices)
+    GrabAndSetArtworkForGivenAppIDs(StreamingServices, AppIDs)
--- Setup.sh
+++
@@ -1,9 +0,0 @@
-#Install the flatpak version of firefox and SGDBoop for non-steam shortcut assets
-flatpak install flathub org.mozilla.firefox flathub com.steamgriddb.SGDBoop -y 
-
-#Move files to default install steam games directory
-mkdir -p /home/$USER/.steam/steam/steamapps/common/Streaming
-cp -a $(pwd)/Streaming /home/$USER/.steam/steam/steamapps/common   
-
-#Run a python script to create the steam shortcuts and set the artwork for each streaming service
-python3 -B CreateShortcuts.py
