901b6ca0a53de54e9ac7d2842426043d6ead51d0
Matthew Hahn <45131687+MatthewHahn73@users.noreply.github.com>
2025-04-24 15:34:37 -0700

    Fixes
    Added a local install of the vdf module for modifying the shortcuts.vdf file

--- CreateShortcuts.py
+++ CreateShortcuts.py
@@ -2,14 +2,17 @@
 CreateShortcuts.py
 
 Current Bugs
-    -Steam doesn't populate the non-steam games with the right artwork. Likely due to steam using some kind of hashing when normally storing those values
+    -Steam doesn't populate the non-steam games with the right artwork. 
+        -Steamgriddb picks a different appid
+            -Still doesn't load some of the artwork? Only the icon
 
 TODO
     -Need to add in better documentation of current script progress and exception handling
     -Need to better optimize the GrabAndSetArtworkForGivenAppIDs function 
 """
 
-import os, binascii, json, logging
+import os, json, logging, random
+import Assets.VDF as vdf
 from urllib.request import Request, urlopen
 
 GlobalLogger = logging.getLogger()
@@ -21,15 +24,14 @@ SteamGameData = "/home/" + User + "/.steam/steam/steamapps/common"
 PathToUserConfig = SteamUserData + os.listdir(SteamUserData)[0] + "/config/grid/"
 PathToSteamShortcutsFile = SteamUserData + os.listdir(SteamUserData)[0] + "/config/shortcuts.vdf"
 
-def GenerateAppID(ExeName, AppName):
-    UniqueID = "".join([ExeName, AppName])
-    AppID = binascii.crc32(str.encode(UniqueID)) | 0x80000000
-    return AppID
+#Generates a random appid
+def GenerateRandomAppID():
+    return random.randint(0, 2147483647)
 
+#Queries steamdb for the artwork for each given streaming service and creates the file in the <User ID>/config/grid directory of the steam user
 def GrabAndSetArtworkForGivenAppIDs(ArtworkLinks, AppIDs):
     for StreamingAppType in ArtworkLinks: 
         for ArtworkType in ArtworkLinks[StreamingAppType]["Artwork"]:
-            #Set the artwork values to something usable and query steamgriddb for the artwork
             ArtworkTypeParsed = ArtworkType.replace(" - Horizontal", "").replace(" - Vertical", "")
             ArtworkURL = "https://cdn2.steamgriddb.com/" + ArtworkTypeParsed + "/" + ArtworkLinks[StreamingAppType]["Artwork"][ArtworkType]
             ArtworkFileName, ArtworkFileType = os.path.splitext(ArtworkURL)
@@ -51,75 +53,61 @@ def GrabAndSetArtworkForGivenAppIDs(ArtworkLinks, AppIDs):
                         })
             ReturnedValue = urlopen(SteamGridDBRequest).read()
 
-            #Create the files in the users userdata/<UserID>/grid/ directory, set to the appropriate AppID
-            FullFileName = AppIDs[StreamingAppType] + ArtworkSuffix + ArtworkFileType
+            FullFileName = str(AppIDs[StreamingAppType]) + ArtworkSuffix + ArtworkFileType
             with open(PathToUserConfig + FullFileName, "wb") as FetchedFile:
                 FetchedFile.write(ReturnedValue)
     
+#Creates a shortcuts file in the steam directory if one doesn't exist. Creates shortcut entries in the vdf file for each streaming service
 def GenerateVDFEntriesAndReturnAppIDs(StreamingServices):
     PathToScript = "bash " + '"/home/' + User + '/.steam/steam/steamapps/common/Streaming/LaunchApp.sh"'
     PathToDirectory = '"/home/' + User + '/.steam/steam/steamapps/common/Streaming/"'
 
-    #Check for an existing shortcuts.vdf file, and if doesn't exist, create one. Determine how many non-steam games already exist
     if not os.path.exists(PathToSteamShortcutsFile):
         logging.info("Creating shortcuts file at '" + PathToSteamShortcutsFile + "'")
-        with open(PathToSteamShortcutsFile, "wb") as NewlyCreatedFile:
-            NewlyCreatedFile.write(b'\x00' + b'shortcuts' + b'\x00\x08\x08')
-    with open(PathToSteamShortcutsFile, "rb") as SteamShortcutsFile:
-        SteamShortcutsFileContentsTrimmed = SteamShortcutsFile.readlines()[0][:-2]
-        CurrentIteration = SteamShortcutsFileContentsTrimmed.count(b'appid')
-        CurrentIteration += 1 if CurrentIteration != 0 else 0
+        with open(PathToSteamShortcutsFile, "wb") as NewlyCreatedShortcutsFile:
+            NewlyCreatedShortcutsFile.write(b'\x00' + b'shortcuts' + b'\x00\x08\x08')
+        NewlyCreatedShortcutsFile.close()
 
-    #For each streaming service, create a non-steam game and populate its attributes
     logging.info("Appending new items to '" + PathToSteamShortcutsFile + "' ...")
-    TotalContentsToAppend = b''
+    ShortcutsDict = vdf.binary_load(open(PathToSteamShortcutsFile, "rb"))
+    CurrentIteration = len(ShortcutsDict["shortcuts"])
     CurrentAppIDList = {}
+
     for Key in StreamingServices:
-        CurrentAppID = str(GenerateAppID("LaunchApp.sh", str(Key))).encode('utf-8')
-        Iteration = str(CurrentIteration).encode('utf-8')
-        AppNameBytes = str(Key).encode('utf-8')                                        
-        PathToScriptBytes = str(PathToScript).encode('utf-8')                            
-        StartDirectoryBytes = str(PathToDirectory).encode('utf-8')      
-        LaunchOptionsBytes = str('"' + str(StreamingServices[Key]["Link"]) + '"').encode('utf-8')   
-
-        CurrentRow = b'\x00' + Iteration + b'\x00' + \
-                        b'\x02' + b'appid' + b'\x00' + CurrentAppID + b'\x00' + \
-                        b'\x01' + b'AppName' + b'\x00' + AppNameBytes + b'\x00' + \
-                        b'\x01' + b'Exe' + b'\x00' + PathToScriptBytes + b'\x00' + \
-                        b'\x01' + b'StartDir' + b'\x00' + StartDirectoryBytes + b'\x00' + \
-                        b'\x01' + b'icon' + b'\x00\x00' + \
-                        b'\x01' + b'ShortcutPath' + b'\x00\x00' + \
-                        b'\x01' + b'LaunchOptions' + b'\x00' + LaunchOptionsBytes + b'\x00' + \
-                        b'\x02' + b'IsHidden' + b'\x00\x00\x00\x00\x00' + \
-                        b'\x02' + b'AllowDesktopConfig' + b'\x00\x01\x00\x00\x00' + \
-                        b'\x02' + b'AllowOverlay' + b'\x00\x01\x00\x00\x00' + \
-                        b'\x02' + b'OpenVR' + b'\x00\x00\x00\x00\x00' + \
-                        b'\x02' + b'Devkit' + b'\x00\x00\x00\x00\x00' + \
-                        b'\x01' + b'DevkitGameID' + b'\x00\x00' + \
-                        b'\x01' + b'DevkitOverrideAppID' + b'\x00\x00' + \
-                        b'\x02' + b'LastPlayTime' + b'\x00\x00\x00\x00\x00' + \
-                        b'\x01' + b'FlatpakAppID' + b'\x00\x00' + \
-                        b'\x00' + b'tags' + b'\x00' + \
-                    b'\x08' + \
-                    b'\x08' 
-        TotalContentsToAppend += CurrentRow
+        GeneratedAppID = GenerateRandomAppID()
+        ShortcutsDict["shortcuts"][str(CurrentIteration)] = {
+                "appid" : GeneratedAppID, 
+                "AppName" : str(Key), 
+                "Exe" : PathToScript, 
+                "StartDir" : PathToDirectory, 
+                "icon" : "",
+                "ShortcutPath" : "", 
+                "LaunchOptions" : str('"' + str(StreamingServices[Key]["Link"]) + '"'), 
+                "IsHidden" : 0, 
+                "AllowDesktopConfig" : 1, 
+                "AllowOverlay" : 1,
+                "OpenVR" : 0,
+                "Devkit" : 0, 
+                "DevkitGameID" : "",
+                "DevkitOverrideAppID" : 0,
+                "LastPlayTime" : 0, 
+                "FlatpakAppID" : "", 
+                "tags" : {}
+            }
         CurrentIteration += 1 
-        CurrentAppIDList[Key] = CurrentAppID.decode('utf-8')
-        
-    #Append the created non-steam games to the existing shortcuts.vdf file
-    with open(PathToSteamShortcutsFile, "wb") as SteamShortcutsFileEnding:
-        SteamShortcutsFileEnding.write(SteamShortcutsFileContentsTrimmed + TotalContentsToAppend + b'\x08\x08')
+        CurrentAppIDList[Key] = GeneratedAppID
+    ShortcutsDictParsed = vdf.binary_dump(ShortcutsDict, open(PathToSteamShortcutsFile, "wb"))
     return CurrentAppIDList
-
+        
 if __name__ == "__main__":
     logging.info("Installing firefox ...")
-    #os.system("flatpak install flathub org.mozilla.firefox -y")
+    os.system("flatpak install flathub org.mozilla.firefox -y")
 
     logging.info("Moving the files to '" + SteamGameData + "' ...")
-    #os.makedirs(SteamGameData + "/Streaming")
-    #os.system("cp -a $(pwd)/Streaming " + "'" + SteamGameData + "'")
+    os.makedirs(SteamGameData + "/Streaming")
+    os.system("cp -a $(pwd)/Streaming " + "'" + SteamGameData + "'")
 
-    with open("Script Assets/StreamingServices.json", "r") as StreamingFile:
+    with open("Assets/StreamingServices.json", "r") as StreamingFile:
         StreamingServices = json.load(StreamingFile)
     AppIDs = GenerateVDFEntriesAndReturnAppIDs(StreamingServices)
     GrabAndSetArtworkForGivenAppIDs(StreamingServices, AppIDs)
